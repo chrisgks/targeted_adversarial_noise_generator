287bba5db64375702f78d2e587785679573bbde5
chr1ss <chris.gkika9619@gmail.com>
2024-05-14 13:41:24 +0100

    integrate BIM and FGSM attack methods

--- src/adversarial_engine_class.py
+++ src/adversarial_engine_class.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+
 from adversarial_helper_class import AdversarialHelper
 
 
@@ -14,18 +15,26 @@ class AdversarialEngine:
         self.original_prediction = None
         self.adversarial_prediction = None
 
-        self.original_image_pil = None
-        self.noise_image_pil = None
-        self.adversarial_image_pil = None
+        self.original_image = None
+        self.perturbation_image = None
+        self.adversarial_image = None
 
         self.original_confidence_score = None
         self.adversarial_confidence_score = None
 
-    def apply_iterative_fgsm_method(self, image, epsilon, target_class, num_steps=15):
+    def _forward_pass(self):
+        pass
+
+    def apply_fgsm_method(self, image, epsilon, target_class, iterations=15):
+
+        # for num_steps=1 this function inmplements the Fast Sign Gradient Method (FGSM)
+        # for num_steps=n, where n>1, this function implements the Basic Iterative Method (BIM) 
+        # by applying iterations of the Fast Sign Gradiend Method (FGSM)
+
         adversarial_image = image.clone().detach()
         adversarial_image.requires_grad = True
 
-        for _ in range(num_steps):
+        for _ in range(iterations):
             output = self.model(adversarial_image)
             loss = -torch.nn.functional.cross_entropy(
                 output, torch.tensor([target_class], device=output.device)
@@ -35,7 +44,7 @@ class AdversarialEngine:
             data_grad = adversarial_image.grad.data
 
             sign_data_grad = data_grad.sign()
-            adversarial_image = adversarial_image + epsilon * sign_data_grad / num_steps
+            adversarial_image = adversarial_image + epsilon * sign_data_grad / iterations
             adversarial_image = torch.clamp(adversarial_image, 0, 1)
 
             adversarial_image = adversarial_image.detach()
@@ -47,7 +56,7 @@ class AdversarialEngine:
         raise NotImplementedError
 
     def visualise_attack(
-        self, epsilon, original_image_pil, noise_image_pil, adversarial_image_pil
+        self, epsilon, original_image, perturbation_image, adversarial_image, iterations
     ):
 
         original_class_name = (
@@ -64,17 +73,17 @@ class AdversarialEngine:
         )
 
         fig, axs = plt.subplots(1, 3, figsize=(15, 8))
-        axs[0].imshow(original_image_pil)
+        axs[0].imshow(original_image)
         axs[0].title.set_text(
             f"Original Prediction \nClass name: {original_class_name}\nConfidence score: {self.original_confidence_score:.3f}%"
         )
         axs[0].axis("off")
 
-        axs[1].imshow(noise_image_pil)
-        axs[1].title.set_text(f"Perturbation (epsilon={epsilon})")
+        axs[1].imshow(perturbation_image)
+        axs[1].title.set_text(f"Perturbation (epsilon={epsilon})\n Iterations: {iterations}")
         axs[1].axis("off")
 
-        axs[2].imshow(adversarial_image_pil)
+        axs[2].imshow(adversarial_image)
         axs[2].title.set_text(
             f"Adversarial Prediction \nClass name: {adversarial_class_name}\nConfidence score: {self.adversarial_confidence_score:.3f}%"
         )
@@ -83,7 +92,7 @@ class AdversarialEngine:
 
         plt.show()
 
-    def perform_adversarial_attack(self, image_path, epsilon, target_class):
+    def perform_adversarial_attack(self, image_path, epsilon, target_class, iterations):
         image = AdversarialHelper.load_and_transform_image_to_tensor(
             image_path=image_path
         )
@@ -92,7 +101,7 @@ class AdversarialEngine:
         self.original_prediction = original_output.max(1, keepdim=True)[1]
         self.original_confidence_score = self.original_probabilities.max().item() * 100
 
-        perturbed_image = self.apply_iterative_fgsm_method(image, epsilon, target_class)
+        perturbed_image = self.apply_fgsm_method(image, epsilon, target_class, iterations)
 
         perturbation = perturbed_image - image
 
@@ -103,7 +112,7 @@ class AdversarialEngine:
             self.adversarial_probabilities.max().item() * 100
         )
 
-        original_image, noise_image, adversarial_image = (
+        original_image, perturbation_image, adversarial_image = (
             AdversarialHelper.transform_tensors_to_images(
                 [image, perturbation, perturbed_image]
             )
@@ -111,7 +120,8 @@ class AdversarialEngine:
 
         self.visualise_attack(
             epsilon=epsilon,
-            original_image_pil=original_image,
-            noise_image_pil=noise_image,
-            adversarial_image_pil=adversarial_image,
+            original_image=original_image,
+            perturbation_image=perturbation_image,
+            adversarial_image=adversarial_image,
+            iterations=iterations
         )
