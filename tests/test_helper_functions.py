ffef06489b247ca70e7cb6f8d6ef7eb8d6a3540d
chr1ss <chris.gkika9619@gmail.com>
2024-05-15 16:12:06 +0100

    get pytest to work with in-app paths

--- tests/test_helper_functions.py
+++ tests/test_helper_functions.py
@@ -1,6 +1,8 @@
 import pytest
 import torch
 
+from PIL.Image import Image
+
 from src.adversarial_helper_class import AdversarialHelper
 
 
@@ -17,7 +19,7 @@ from src.adversarial_helper_class import AdversarialHelper
     ],
 )
 def test_load_and_transform_image_to_tensor(
-    image_path: str, expected: bool, testing_scenario
+    image_path: str, expected: bool, testing_scenario: str
 ):
     try:
         result = isinstance(
@@ -30,9 +32,26 @@ def test_load_and_transform_image_to_tensor(
     assert result == expected, testing_scenario
 
 
+@pytest.mark.parametrize(
+    "tensors,expected,testing_scenario",
+    [
+        ([torch.rand(224, 224)], True, "tensor transformed to image successfully"),
+        (
+            [torch.rand(100, 50), torch.rand(50, 100)],
+            True,
+            "tensors transformed to images successfully",
+        ),
+    ],
+)
 @pytest.mark.unittest
-def test_transform_tensors_to_images():
-    pass
+def test_transform_tensors_to_images(
+    tensors: list, expected: bool, testing_scenario: str
+):
+    for t in tensors:
+        assert t.shape[0] > 1, "tensor should have at least 2 dimentions"
+    result = AdversarialHelper.transform_tensors_to_images(tensors)
+    for r in result:
+        assert isinstance(r, Image) == expected, testing_scenario
 
 
 @pytest.mark.unittest
@@ -46,7 +65,7 @@ def test_transform_tensors_to_images():
     ],
 )
 def test_load_torchvision_pre_trained_model(
-    model_name: str, expected: bool, testing_scenario
+    model_name: str, expected: bool, testing_scenario: str
 ):
     try:
         assert (
@@ -58,6 +77,8 @@ def test_load_torchvision_pre_trained_model(
         pass
 
 
+@pytest.mark.skip
 @pytest.mark.unittest
 def test_load_imagenet_classes():
-    pass
+    result = AdversarialHelper.load_imagenet_classes()
+    assert isinstance(result, list) == True, "imagenet classes should be a list"
