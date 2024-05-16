95bba9e22cd7e35a1e7b6ae8b812443a61ee8e15
chr1ss <chris.gkika9619@gmail.com>
2024-05-16 05:32:27 +0100

    add install script and requirements.txt

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
-conda create -n adversarial_engine python=3.12 -y
+conda create -n adversarial_engine python=3.12.3 -y
 conda activate adversarial_engine
+
 pip install -r requirements.txt
+
+# run all tests
 pytest
+# run an example
 python src/run_adversarial_engine.py notebooks/data/example_images/vito1.jpg 291
+
+conda deactivate
 conda env remove -n adversarial_engine
